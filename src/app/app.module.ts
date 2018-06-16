cc2a2cca1db9888cd9165749b63860eb31cbc2e2
Amit Badiyal <amitbadiyal@Amits-MacBook-Pro.local>
2018-06-16 12:02:34 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { FaqComponent } from './faq/faq.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FaqComponent } from './faq/faq.component';
     AboutUsComponent,
     HomeComponent,
     ContactUsComponent,
-    FaqComponent
+    FaqComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,AppRoutingModule
